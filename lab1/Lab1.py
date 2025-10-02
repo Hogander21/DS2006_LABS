ae6c52236249069de91ba4b20116ef6cc040470a
Karl Hogander <karl.hogander@gmail.com>
2025-10-02 15:20:24 +0200

    Lab1 commit

---
+++ lab1/Lab1.py
@@ -0,0 +1,46 @@
+print("Welcome to Python Programming")
+
+result_addition = 2 + 6
+print("The result of multiplication is:", result_addition)
+
+result_multiplication = 5 * 3
+print("The result of the multiplication is:", result_multiplication)
+
+print("Hello World")
+
+A = 10
+B = 20 
+
+print("The result of the addition is", A + B)
+print("The result of the additon is ", A * B)
+print("The result of the divison is ", A/B)
+
+name = "Bob"
+age = 28
+
+print(f"Hi! My name is", {name}, "And I am", {age}, "yearls old")
+print(f"In 2 years, I will be {age + 2} years old")
+
+if age < 30:
+    print(f"access granted. Welcome {name}.")
+else:
+    print(f"Access refused. Common {name}, you can come back in {30 - age} years.")
+
+usernames = ["Mark", "Sara", "Ahmed", "Johanna"]
+
+for user in usernames:
+    print(f"Welcome {name}")
+
+for i in range(0,10):
+    print(f"The Double of {i} is {2 * i}")
+
+pseudoname = input("Please enter your pseudoname: ")
+print("Hello", pseudoname)
+
+print("The variable A is ", A)
+print("The variable B is ", B)
+print("The variable name is ", name)
+print("The variable age is ", age)
+print("The variable pseudoname is ", pseudoname)
+print("The variable username is ", usernames)
+
